b6ea9f79e306eb06060deb6155b2450607c240df
Sameem S <the.infinite.minded@gmail.com>
2026-02-25 14:10:36 +0530

    fix: invalid base endpoint

--- main.go
+++ main.go
@@ -13,7 +13,7 @@ func main() {
 		port = "3000"
 	}
 
-	http.HandleFunc("", func(w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintln(w, "Your deployment is working!")
 	})
 	http.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
